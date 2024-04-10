9e1b8d6f29b4ba293333d78309681e3775bca0cf
DaironRv <dairon.rodriguezvega@gmail.com>
2024-04-09 19:19:21 -0500

    ajustes y cmambio de fondo

---
+++ src/components Games/JavaScript/game1.js
@@ -0,0 +1,9 @@
+// const nombre = 'Dairon'
+
+// console.log(`hola que hace ${nombre}`)
+
+
+// el = document.addEventListener('DOMContentLoaded',function(){}). Es para que el Script se carge despues de carga del html, asi todo ira por passos sin necesidad de cargar primero java script
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('Hola que hace')
+})
