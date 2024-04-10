27b86f30b2c41fe52a845a82e44a3ed017350aa2
DaironRv <dairon.rodriguezvega@gmail.com>
2024-04-09 19:20:48 -0500

    delete Games demo

--- src/components Games/JavaScript/game1.js
+++
@@ -1,9 +0,0 @@
-// const nombre = 'Dairon'
-
-// console.log(`hola que hace ${nombre}`)
-
-
-// el = document.addEventListener('DOMContentLoaded',function(){}). Es para que el Script se carge despues de carga del html, asi todo ira por passos sin necesidad de cargar primero java script
-document.addEventListener('DOMContentLoaded',function(){
-    console.log('Hola que hace')
-})
