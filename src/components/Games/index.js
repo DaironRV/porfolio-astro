8deac69797e927e6576e5e8f8529b2cb4d4ecd3a
DaironRv <dairon.rodriguezvega@gmail.com>
2024-04-26 19:52:21 -0500

    Add initial practic fron game page

---
+++ src/components/Games/index.js
@@ -0,0 +1,14 @@
+
+// esta es una funcion normal en java scrip
+var start = document.getElementById('start')
+
+start.addEventListener('click',function(){
+    bucleInfinito.bucle()
+})
+
+// esto es para eviata siempre escibir function en java script
+var start = document.getElementById('start')
+
+start.addEventListener('click',()=>{
+    bucleInfinito.bucle()
+})
